dc594122895506710fe93ee386ecc685561da419
Thomas Haygarth <haygarth.thomas@gmail.com>
2018-08-14 01:38:30 +0100

    Added a simple player tracker class that sets up a focal point between all the players

---
+++ Assets/Scripts/Camera/PlayerTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OuttaSpace.Camera {
+	public class PlayerTracker : MonoBehaviour {
+
+		public Transform focalTransform;
+		public Transform[] players;
+
+		private const float zDepth =-10.0f;
+		
+		// Update is called once per frame
+		void Update () {
+			var length = players.Length;
+			var focalPoint = Vector3.zero;
+
+			for(int i = 0; i < length; ++i) {
+				focalPoint += players[i].position;
+			}
+
+			focalPoint.z = zDepth;
+
+			focalTransform.position = focalPoint / length;
+		}
+	}
+}
